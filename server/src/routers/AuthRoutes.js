7b48ce35c34d103a978da5a93ccffd67eecc1160
Anup Chauhan <anupchauhanak6@gmail.com>
2025-11-26 13:42:51 +0530

    add AuthRoutes for user authentication endpoints

---
+++ server/src/routers/AuthRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+const router=express.Router();
+import authController from "../controllers/authController";
+
+router.post('/send/login-signup-otp', authController.sendLoginOtp);
+
+router.post('/signup', authController.createUser);
+
+router.post('/signin', authController.sigin);
+
+export default router;
