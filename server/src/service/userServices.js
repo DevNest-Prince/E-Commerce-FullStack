1edd030c7eef1be6a6fe78594e7568d9e3285dce
Anup Chauhan <anupchauhanak6@gmail.com>
2025-11-27 12:02:27 +0530

    feat: implement user services for profile and email retrieval

--- server/src/service/userServices.js
+++ server/src/service/userServices.js
@@ -1,1 +1,30 @@
 import User from "../models/User.js";
+import jwtProvider from "../utils/jwtProvider.js";
+import UserError from "../errors/UserError.js";
+
+class UserServices{
+
+    async findUserProfileByJwt(jwt){
+        const email = jwtProvider.getEmailFromjwt(jwt)
+
+        const user = await User.findOne({email})
+
+        if(!user) {
+            throw new UserError(`User does not exist ${email}`);
+        }
+
+        return user;
+    }
+
+    async findUserByEmail(email){
+        const user = await User.findOne({email})
+
+        if (!user) {
+            throw new UserError(`User does not exist with email: ${email}`);
+        }
+
+        return user;
+    }
+}
+
+export default new UserServices();
