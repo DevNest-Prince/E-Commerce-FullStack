a3c28575a8eceba841b8f575ff56e8166e7657ee
Prince Kothari <197196645+princekotharii@users.noreply.github.com>
2025-11-27 20:57:56 +0530

    feat: enhance ProductService with CRUD operations and search functionality

--- server/src/service/ProductService.js
+++ server/src/service/ProductService.js
@@ -1,4 +1,5 @@
-import Category from "../models/Category";
+import Category from "../models/Category.js";
+import Product from "../models/Product.js";
 
 const calculateDiscountPercentage=(mrpPrice ,sellingPrice)=>{
     if(mrpPrice<=0){
@@ -26,9 +27,23 @@ class ProductService{
             
             const product=new Product({
                 title:req.title,
-                description:req.description
-            })
-        } catch (error) {}
+                description:req.description,
+                images:req.images,
+                sellingPrice:req.sellingPrice,
+                mrpPrice:req.mrpPrice,
+                discountPresent,
+                size:req.sizes,
+                color:req.color,
+                quantity:req.quantity,
+                seller:seller._id,
+                category:category3._id,
+            });
+
+            return await product.save();
+
+        } catch (error) {
+            throw new Error(error.message)
+        }
     }
 
     async createOrGetCategory(categoryId, lavel , parentId=null){
@@ -37,7 +52,7 @@ class ProductService{
         if(!category){
             category =new Category({
                 categoryId,
-                lavel,
+                level:lavel,
                 parentCategory:parentId
             });
            category = await category.save();
@@ -45,4 +60,103 @@ class ProductService{
         return category;
 
     }
+
+    async deleteProduct(productId){
+        try{
+            await Product.findByIdAndDelete(productId);
+            return "Product deleted Successfully";
+        } catch(error){
+            throw new Error(error.message)
+        }
+    }
+
+    async updateProduct(productId, updateProductData){
+        try {
+            const product =await Product.findByIdAndUpdate(productId, updateProductData ,{new:true});
+            return product;
+        } catch (error) {
+            throw new Error (error.message)
+        }
+    }
+
+    async findProductById(productId){
+        try {
+            const product =await Product.findById(productId);
+            if(!product){
+                throw new Error("Product not found")
+            }
+            return product;
+        } catch (error) {
+            throw new Error(error.message)
+        }
+    }
+
+
+    async searchProduct(query){
+        try {
+            const products =await Product.find({title: new RegExp(query,"i")});
+            return products
+        } catch (error) {
+            throw new Error(error.message)
+        }
+    }
+
+    async getProductBySellerId(sellerId){         
+        return await Product.find({seller:sellerId});
+    }
+    
+    async getAllProducts(req){
+        const filterQuery={};
+        if(req.category){
+            const category=await Category.findOne({categoryId:req.category});
+
+            if(!category){
+                return {
+                    content:[],
+                    totalpages:0,
+                    totalElement:0
+                }
+            }
+            filterQuery.category=category._id.toString();
+        }
+
+        if(req.color){
+            filterQuery.color=req.color;
+        }
+
+        if(req.minPrice && req.maxPrice){
+            filterQuery.sellingPrice={$gte:req.minPrice,$lte:req.maxPrice};
+        }
+        if(req.minDiscount){
+            filterQuery.discountPresent={$gte:req.minDiscount};
+        }
+        if(req.size){
+            filterQuery.size=req.size;
+        }
+        let sortQuery={};
+        if(req.sort=="price_low"){
+            sortQuery.sellingPrice=1;
+        }else if(req.sort=="price_high"){
+            sortQuery.sellingPrice=1;
+        }
+
+        const product=await Product.find(filterQuery)
+        .sort(sortQuery)
+        .skip(req.pageNumber*10)
+        .limit(10);
+
+        const totalElement=await Product.countDocuments(filterQuery);
+
+        const totalpages=Math.ceil(totalElement/10)
+
+        const res={
+            content:product,
+            totalpages:totalpages,
+            totalElement:totalElement
+        }
+        return res;
+
+    }
 }
+
+export default new ProductService();
